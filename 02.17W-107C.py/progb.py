8332efcd1b8071a7d06892bce5ae81b566072592
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-09-27 23:10:22 -0300

    Revisão as

--- 02.17W-107C.py/progb.py
+++ 02.17W-107C.py/progb.py
@@ -1,4 +1,10 @@
-print('Begin', __name__)
+"""
+A grende sacada do módulo
+"""
+
+
+
+"""print('Begin', __name__)
 import proga
 def main():
     print('enrei no main')
@@ -14,7 +20,16 @@ print('End', __name__)
 
 if __name__ == '__main__':
     main()
-    
+    """
 # quando execultado ou importado um módulo, o interpretador Python compila o código fonte (arquivo .py) em bytecode, que é um formato intermediário mais próximo do código de máquina. Isso facilita a execução mais rápida do código. 
 # Os arquivos compilados (.pyc) são armazenados no diretório __pycache__. Da próxima vez que você importar o módulo, o Python pode carregar o bytecode compilado diretamente a partir deste diretório, em vez de recompilar o código fonte, o que melhora a eficiência.
 
+
+
+"""
+print(90*'-')
+
+import proga
+proga.fa()
+
+"""
