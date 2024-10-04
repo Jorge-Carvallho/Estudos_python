3dbdfef0aad878ca84701a585e176bfb60027321
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-10-03 21:22:45 -0300

    Adicionando exercicios de manipulando strings

--- 04.19W-110C.py
+++ 04.19W-110C.py
@@ -54,3 +54,52 @@ print(nome + resultado,' resultado')
 print(nome + resultado1, 'resultado 1')
 print(nome + resultado2, ' resultado 2')
 
+print('---------------------------------------------------------------------------')
+
+# Exercícios:
+# Exercício 1: Crie uma variável chamada palavra e atribua a ela a string "linguagem de programação". Use os métodos de string para:
+# Imprimir a string toda em maiúsculas.
+# Imprimir a string com a primeira letra da primeira palavra em maiúscula.
+# Imprimir a string com todas as primeiras letras de cada palavra em maiúsculas.
+# Substituir todas as ocorrências da letra "a" por "A".
+# Dividir a string em uma lista de palavras, utilizando o espaço como delimitador.
+palavra = 'linguagem de programação'
+print(palavra.upper())
+print(palavra.capitalize())
+print(palavra.title())
+print(palavra.replace('a', 'A'))
+print(palavra.split(' '))
+
+# Exercício 2: Crie uma variável chamada nome_completo e atribua a ela a string "Maria Fernanda da Silva". Utilize o método split para dividir o nome completo em uma lista de palavras. Depois, use o método join para concatenar essas palavras novamente, mas separando-as por um hífen (-).
+nome_completo = 'Maria Fernanda da Silva'
+lista_de_nomes = nome_completo.split(' ')
+print(lista_de_nomes)
+lista_de_nomes = ' '.join(lista_de_nomes)
+print(lista_de_nomes)
+
+# Exercício 3: Dada a string "Python é poderoso e versátil", faça o seguinte:
+# Imprima a string com todas as letras em minúsculas.
+# Substitua todas as ocorrências da palavra "poderoso" por "impressionante".
+# Divida a string em uma lista de palavras, utilizando o espaço como delimitador.
+# Imprima o comprimento da string.
+string = 'Python é poderoso e versátil'
+print(string.lower())
+print(string.replace('poderoso','impressionante'))
+print(string.split(' '))
+print(len(string))
+
+# Exercício 4: Crie uma frase de sua escolha e faça as seguintes operações:
+# Verifique o comprimento da frase.
+# Concatene a frase com outra frase de sua escolha, utilizando o operador +.
+# Substitua todas as ocorrências da letra "o" por "0" (zero).
+# Divida a frase em uma lista de palavras.
+# Use o método join para unir as palavras com um espaço entre elas e imprima o resultado.
+frase = 'Aqui estão alguns exercícios para você praticar, baseados no conteúdo sobre strings e seus métodos'
+print(len(frase))
+print('Esse texto esta comcatenado com, ' +  frase)
+print(frase.replace('o','0'))
+frase_lista = frase.split()
+print('Frase em lista ',  frase_lista)
+frase_lista= ' '.join(frase_lista)
+
+print(frase_lista)
