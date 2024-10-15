9dc2a696aa51b16cd597c61f6d74f2dbceb09335
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-10-14 22:23:55 -0300

    adicionando exercíciios de for, while com enumerate() e zip()

--- 09.25W-116C.py
+++ 09.25W-116C.py
@@ -64,3 +64,60 @@ print(numeros)
 # Escreva um código que peça uma string ao usuário e, usando enumerate(), imprima o índice e o caractere correspondente em cada iteração do loop.
 for i, caracter in enumerate(string):
     print(f'O indice é --> {i} e caracter é -->  {caracter}')
+    
+print('-----------------------------------------Exercicio for------------------------------------\n')
+
+# Escreva um programa que exiba todos os números pares de 1 a 50 (inclusive). Use um loop for e a função range().
+for numeros in range(2,51,2):
+    print(numeros)
+    print('----------------------------------------------------------  ')
+    
+# Escreva um programa que peça ao usuário um número inteiro entre 1 e 10 e mostre a tabuada desse número de 1 a 10. Use um loop for e a função range().
+numero = 3
+for i in range(1,11):
+    print(f'{numero} x {i} = {numero * i}')
+    
+    
+print('-------------------------------------')
+i = 1
+while i <= 10:
+   print(f'{numero} x {i} = {numero * i }')
+   i +=1
+   
+print('--------------------------------')
+# Escreva um programa que exiba a soma de todos os múltiplos de 3 entre 1 e 100 (inclusive). Utilize um loop for e a função range().
+
+soma = 0
+for i in range(3,100,3):
+    soma += i
+print(f'A soma de todos os multiplos de 3 é {soma}' )
+
+print('-----------------------------------------')
+# Escreva um programa que faça uma contagem regressiva a partir de um número fornecido pelo usuário até 0. O programa deve exibir cada número da contagem. Use um loop for e a função range().
+numero_inicial = 2
+for i in range(numero_inicial,-1,-1):
+    print(i)
+print('-------------------------------------------------')
+# Escreva um programa que receba uma lista de itens de compras (como frutas, legumes, etc.) e exiba cada item da lista junto com seu número de ordem (começando do número 1). Utilize for e enumerate().
+
+lista = ['banana', 'maçã', 'laranja', 'abacaxi']
+i
+for i, fruta in enumerate(lista,start=1):
+   
+    print(i, fruta)
+    
+print('------------------------------------------------------')
+
+# Escreva um programa que receba uma lista com as notas de 5 alunos e exiba cada nota junto com a posição do aluno na lista (começando do número 1).
+notas_uni =[2.4,4.6,10,5.7,7.7] 
+for notas_uni, estud in enumerate(notas_uni, start=1):
+    print(notas_uni,estud)
+
+
+
+
+print('-----------------------------exercícios idéia-----------------------')
+notas = [7.5, 8.0, 9.2, 6.7, 8.5]
+estudantes = ['Jorge','Fábio', 'Jéssica', 'Evandro', 'Juan']
+for notas, estudantes in zip(estudantes,notas):
+    print(estudantes, notas)
