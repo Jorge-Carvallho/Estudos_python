e42265f074782591b40ab8810b0645c8a9a59f2f
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-11-21 15:03:05 -0300

    Adicionado exercício guanabara 107

--- exer-modulo-guanabara-107/moeda.py
+++ exer-modulo-guanabara-107/moeda.py
@@ -2,25 +2,48 @@
 # Crie um módulo chamado moeda.py que tenha funções incorporadas aumentar(),diminuir(),
 # dobro(), e metade().
 # Faça também um programa principal que importe esse módulo e use algumas dessas funções
-import decimal
-def metade(n):
-    n /= 2 
-    return f'{n:g}'
+
+    # --------> usando import decimal < -------------#
+# import decimal
+# def metade(n):
+#     n /= 2 
+#     return f'{n:g}'
 
 
-def dobro(n):
-    n *= 2
-    return f'{n:g}'
+# def dobro(n):
+#     n *= 2
+#     return f'{n:g}'
 
 
-def aumentar(n):
-    n *= 1.40
-    return f'{n:g}'
+# def aumentar(n):
+#     n *= 1.40
+#     return f'{n:g}'
     
 
-def reduzir(n):
-    n *= 0.80
-    return f'{n:g}'
+# def reduzir(n):
+#     n *= 0.80
+#     return f'{n:g}'
+
+     # ------------> sem o uso do decimal <-------------------#
+
+def aumentar(preço,taxa):
+    res = preço + (preço * taxa/100)
+    return res
+
+
+def diminuir(preço, taxa):
+    res = preço - (preço * taxa/100)
+    return res
+
+
+def dobro(preço):
+    res = preço * 2
+    return res
+
+
+def metade(preço):
+    res = preço / 2
+    return res
 
 
 
