6abef629080f711a8c7513cb24b4cc58a0915548
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-11-24 19:19:17 -0300

    Iniciando exercício 115 guanabara

---
+++ exer115/sistema.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('/home/jorge/Documentos/wttd_aulas') 
+# Adiciona o diretório '/home/jorge/Documentos/wttd_aulas' ao caminho de busca de pacotes do Python
+# (sys.path), permitindo que o Python encontre o pacote 'exer115'
+# mesmo que o script esteja sendo executado de outro diretório.
+
+from exer115.lib.interface import *
+
+cabeçalho('SISTEMA ARQUIVO v1.0')
