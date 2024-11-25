3ff84e80905a5b70e58539e04ae85616a94436c7
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-11-25 06:24:05 -0300

    Adicionado funções do Menu princiapal

--- exer115/lib/interface/__init__.py
+++ exer115/lib/interface/__init__.py
@@ -1,3 +1,19 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError) as e:
+            print(f'\033[31mError, Digite um número válido: \033[m ')
+            # print(f'\033[31mError, Digite um número válido: --> {e.__class__}\033[m] ')
+            continue
+        except KeyboardInterrupt as e :
+            print(f'\n\033[31mUsuário preferio não digitar nenhum valor: \033[m ')
+            # print(f'\n\033[31mUsuário preferio não digitar nenhum valor --> {e.__class__}\033[m] ')
+            return 0
+        else:
+            return n
+        
+        
 def linha(tam=42):
     return '-' * tam
 
@@ -6,3 +22,14 @@ def cabeçalho(txt):
     print(linha())
     print(txt.center(42))
     print(linha())
+    
+
+def menu(lista):
+    cabeçalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c +=1
+    print(linha())
+    opc = leiaInt('\033[32mSua Opção: \033[m')
+    return opc
--- exer115/sistema.py
+++ exer115/sistema.py
@@ -5,5 +5,20 @@ sys.path.append('/home/jorge/Documentos/wttd_aulas')
 # mesmo que o script esteja sendo executado de outro diretório.
 
 from exer115.lib.interface import *
+from time import sleep
+
 
 cabeçalho('SISTEMA ARQUIVO v1.0')
+
+while True:
+    resposta = menu(['Ver pessoas cadastadas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        cabeçalho('Opção 1')
+    elif resposta == 2:
+        cabeçalho('Opção 2')
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida! \033[m')
+    sleep(0.9)
