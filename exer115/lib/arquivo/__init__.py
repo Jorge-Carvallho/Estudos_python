ee51b3560160ec992079908dd8362824b81e443b
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-11-27 07:12:21 -0300

    Adicionando teste do inicio do exercicio 115b, não estava achando o arquivo quando ele era criado

--- exer115/lib/arquivo/__init__.py
+++ exer115/lib/arquivo/__init__.py
@@ -1,8 +1,39 @@
+# def arquivoExiste(nome):
+#     print(f'Verificando o arquivo: {nome}')
+#     try:
+#         a = open(nome, 'rt')
+#         a.close()
+#     except FileNotFoundError:
+#         return False
+#     else:
+#         return True
+    
+    
+    
 def arquivoExiste(nome):
+    print(f'Verificando o arquivo {nome}')
+
     try:
-        a = open(nome,'rt')
+        a = open(nome, 'rt')
         a.close()
-    except FileNotFoundError:
+        print('Arquivo encontrado com rt')
+
+    except FileNotFoundError as e:
+        print(f'Error com rt --> {e}')
         return False
-    else:
-        return True
+    
+    try:
+        a = open(nome, 'rb')
+        a.close()
+        print('Arquivo encontrado com rb')
+
+    except FileNotFoundError as e:
+        print(f'ERRor com rb --> {e}')
+        return False
+
+    except Exception as e:
+        print(f'Outro erro com rb {e}')
+
+    return True
+        
+    
