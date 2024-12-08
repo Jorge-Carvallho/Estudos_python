5e37395bff3c2dc564cda55b05e119c0010dd152
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-12-07 23:25:52 -0300

    Adicionando exercício dwe Estrutura de repetição FOR

---
+++ exer46-contagem-regressiva.py
@@ -0,0 +1,9 @@
+# Exercicio 46
+# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artificio
+# indo de 10 ate 0 com uma pausa de 1 segundo entre eles
+from time import sleep
+
+for c in range(10,-1,-1):
+    print(c)
+    sleep(0.7)
+print('bomm')
