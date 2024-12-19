e45ae9f18dcc6a558c048d7b6245405e34c13949
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-12-18 23:38:45 -0300

    Adicionando exercício interrompendo repetiçãi while

---
+++ exer71-0simulador-caixa-eletronico.py
@@ -0,0 +1,67 @@
+# Exercício 71
+# Crie um programa que simule o funcionamento de um caixa eletronico.
+# No inicio pergunte ao usuário qual sera o valor a ser sacado número inteiros
+# e o programa  vai informa quantas cédulas de cada valor serão entregues.
+
+# OBS: Considere que o caixa possui Cédulas de 50,20,10,1 real.
+
+
+
+
+# valor = int(input('Qaul o valor voce quer sacar: '))
+# total = valor
+# ced = 50
+# totalced = 0
+# while True:
+#     if total >=  ced:
+#         total -= ced
+#         totalced += 1 
+#     else:
+#         if totalced > 0:
+#             print(f'Total de {totalced} cédulas de {ced}')
+#         if ced == 50:
+#             ced = 20
+#         elif ced == 20:
+#             ced = 10
+#         elif ced == 10:
+#             ced = 1
+#         totalced = 0
+#         if total == 0:
+#             break
+        
+        # -------------------------------------resolução -------------------------------
+        
+# valor = int(input('Qual valor quer sacar: '))
+# total = valor
+# ced = 50
+# totalced = 0
+# while True:
+#     if  total >= ced:
+#         total -= ced
+#         totalced += 1
+#     else: 
+#         if totalced > 0:
+#             print(f'total de cedulas {totalced}  celdulas de {ced}')
+#         if ced == 50:
+#             ced = 20
+#         elif  ced == 20:
+#             ced = 10
+#         elif ced == 10:
+#             ced = 1
+#         totalced = 0
+#         if total == 0:
+#             break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
