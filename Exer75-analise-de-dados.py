28eedb80f601cda2235b3ab91916c24f12386f77
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2024-12-21 21:13:04 -0300

    Adicionado exercício de tuplas

--- Exer75-analise-de-dados.py
+++ Exer75-analise-de-dados.py
@@ -1,14 +1,32 @@
+# Exercício 75
+# Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.
+# No final mostre:
+#     a) Quantas vezes apareceu o valor 9
+#     b)Em que posição foi digitado o primeiro valor 3 
+#     c)Quais foram os números pares
+
 numero = (
     int(input('Digite primerio número: ')),
     int(input('Digite segundo número: ')),
     int(input('digite o terceiro valor: ')),
     int(input('Digite o quarto valor: '))
-    
 )
 
-# numero = (
-# )
+print(f'Você digitou os valores {numero}')
 
-# print(f'Você digitou os valores {numero}')
+print(f'O número 9 apareceu, {numero.count(9)} vezes')
+if 3 in numero:
+    print(f'O número 3 apareceu na {numero.index(3)}ª posição')
+else:
+    print('Valor 3 não foi digitado')
+print(f'Os números pares foram: ',end = '') 
+for c in numero:
+  if c % 2 == 0:
+    print(c,end=' ')
+print()
 
-print(f'Apareceu o numero 9, {numero.count(9)} vezes')
+
+    
+
+
+    
--- exer76-lista-de-preços-com-tuplas.py
+++ exer76-lista-de-preços-com-tuplas.py
@@ -5,9 +5,27 @@
 
 
 produtos = ('Lápis', 1.75, 'Borracha', 2.00, 'caderno',15.90, 'estojo',25.90, 'trasferidor', 4.20,
-            'Compasso',9.99, 'mochila', 120.32, 'canetas',22.30 ,'livros', 34.90)
+            'Compasso',9.99, 'mochila', 120.32, 'canetas',22.30 ,'livros', 34.90
+            )
 
-print('---'*17)
-print(f'{'LISTAGEM DE PREÇOS':^46}')
-print('---'*17)
+print('--'*20)
+print(f'{'LISTAGEM DE PREÇOS':^40}')
+print('--'*20)
+# cont = 0
 
+# for i in range(0, len(produtos),2):
+#     nome = produtos[i]
+#     preco = produtos[i + 1]
+     
+#     print(f'{nome:<11} ------------------------------ R${preco:>9} ')
+    
+# print('-' * 55)
+# # -------------------------------------resolução-------------------------------------
+
+
+for pos in range(0,len(produtos)):
+    if pos % 2 == 0:
+        print(f'{produtos[pos]:.<30}', end= '')
+    else:
+        print(f'R$ {produtos[pos]:>7.2f}')
+print('--'* 20)
