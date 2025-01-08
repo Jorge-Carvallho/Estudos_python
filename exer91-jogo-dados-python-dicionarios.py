5948d04c4d052a899214bc599fba03d931b807d3
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2025-01-07 23:57:18 -0300

    Adicionando aula e exercícios de funções

--- exer91-jogo-dados-python-dicionarios.py
+++ exer91-jogo-dados-python-dicionarios.py
@@ -1,7 +1,7 @@
 # Exercício 91
-# Crie um programa onde 4 jogadores joguem um dadoe tenham resultados aleatórios
-# Guarde esse reusltado em um dicionário.
-# No final coloque esse dicionário em orden sabendo que o que o vencedortirou o maior número no dado
+# Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios
+# Guarde esse resultado em um dicionário.
+# No final coloque esse dicionário em orden sabendo que o que o vencedor tirou o maior número no dado
 
 # from random import randint
 # from operator import itemgetter
