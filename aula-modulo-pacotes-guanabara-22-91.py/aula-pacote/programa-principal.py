1fb5f9ca9379d8dabc11678535fface0f0e6770b
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2025-01-18 20:46:34 -0300

    Retirei aula de Módulo

--- aula-modulo-pacotes-guanabara-22-91.py/aula-pacote/programa-principal.py
+++
@@ -1,8 +0,0 @@
-from uteis import numeros
-
-
-num= int(input('Digite em valor '))
-fat=numeros.fatorial(num)
-print(f'O fatorial de {num} é {fat}')
-print(f'O Dobro de {num} é {numeros.dobro(num)}')
-print(f'O Triplo de {num} é {numeros.triplo(num)}')
