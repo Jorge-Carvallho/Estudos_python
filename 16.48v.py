000a88585796d29796587664c9a0413e07941ed5
Jorge-Carvallho <jorge.carvalho0309@hotmail.com>
2025-01-18 20:40:04 -0300

    Removendo aulas de python para deixar apenas Exercícios

--- 16.48v.py
+++
@@ -1,7 +0,0 @@
-# Raw String
-import re
-#  Raw string impedem que caracteres de controle sejam interpretados e os caracteres sejam tratados como string ex:
-text = '\\section\n'
-print(re.match('\\section', 'text'))# retornou None pois o \\s foi interpretado como caracter de controle
-print(re.match(r'\\section', text))# agora usando o  --> r <-- a frente   --> '' <--ele é interpretado apenas como string
-print(re.findall(r'\\section', text))
